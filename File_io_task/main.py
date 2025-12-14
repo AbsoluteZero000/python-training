8cc100ea8b6969c0b008c77206118160e5a4bd25
Ahmed Wael Wanas <ahmedwaelwanas@gmail.com>
2025-12-14 09:17:28 +0200

    feat: added output file for the errors

--- File_io_task/main.py
+++ File_io_task/main.py
@@ -1,17 +1,13 @@
-import os
-
-
-
 with open("server-log.txt", "r") as file:
     with open("errors-only.txt", "w") as output_file:
         counter = 0
         for line in file:
             data = line.strip()
             if "ERROR" in data:
-                print("Error found in log:", data)
                 output_file.write(data + "\n")
                 counter += 1
 
 
         print("The number of errors found in the logs are " + str(counter))
+
 
